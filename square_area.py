66fe708e517220fafb0509464540d40e32d610d8
dzikrilaziz02 <dzikril.aziz_ti24@nusaputra.ac.id>
2025-03-02 14:50:26 +0700

    initial commit

---
+++ square_area.py
@@ -0,0 +1,9 @@
+#program to calculate the area of a square
+#step 1: get user input for the side lenght
+side = float(input("Enter the length of one side of the square:"))
+
+#step 2:calculate the area
+area = side **2
+
+# step 3: Display the result
+print(f"The area of the square is: {area}")
